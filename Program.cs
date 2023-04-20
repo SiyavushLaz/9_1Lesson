1b1bfd4f023405767d46127982a692a570847fdc
Siyavush-L <s.lazgiyev@gmail.com>
2023-04-20 15:38:23 +0600

    Gotov

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        Console.Write("Введите значение N: ");
+        int n = int.Parse(Console.ReadLine());
+        PrintNumbers(n);
+    }
+
+    static void PrintNumbers(int n)
+    {
+        if (n < 1)
+            return;
+        Console.Write("{0} ", n);
+        PrintNumbers(n - 1);
+    }
+}
